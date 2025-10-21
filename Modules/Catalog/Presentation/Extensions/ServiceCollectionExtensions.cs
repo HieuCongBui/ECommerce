6fb04441a7175f635a471e1c9acd5c0390938ff1
buihieudl <buihieudl@gmail.com>
2025-10-21 16:35:02 +0700

    Update Extensions for Presentation layer

---
+++ Modules/Catalog/Presentation/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Ecommerce.Catalog.Infrastructure.Persistence;
+using Ecommerce.Shared.DataAccess.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Catalog.Presentation.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCatalogModule(
+        this IServiceCollection services)
+        {
+            // Add DbContext with module-specific schema
+            services.AddModuleDatabase<CatalogDbContext>(nameof(Catalog), "catalog");
+            return services;
+        }
+    }
+}
